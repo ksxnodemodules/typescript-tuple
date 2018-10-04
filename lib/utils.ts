3b5b53c3cebdce2d84651b071cdadc5717169565
khai96_ <hvksmr1996@gmail.com>
2018-10-04 14:42:11 +0700

    Remove all unnecessary commas

--- lib/utils.ts
+++ lib/utils.ts
@@ -22,10 +22,10 @@ export type SplitInfiniteTuple<Tuple extends any[]> =
   : never
 
 export type _SplitInfiniteTuple<Tuple extends any[], Holder extends any[] = []> = {
-  matched: [Holder, Tuple],
+  matched: [Holder, Tuple]
   unmatched: ((..._: Tuple) => any) extends ((_: infer First, ..._1: infer Rest) => any)
     ? _SplitInfiniteTuple<Rest, Prepend<Holder, First>>
-    : never,
+    : never
   finite: [Tuple, []]
 }[
   Tuple extends (infer Element)[] ?
@@ -34,9 +34,9 @@ export type _SplitInfiniteTuple<Tuple extends any[], Holder extends any[] = []> 
 ]
 
 export type Last<Tuple extends any[], Default = never> = {
-  empty: Default,
-  single: Tuple extends [infer SoleElement] ? SoleElement : never,
-  multi: ((..._: Tuple) => any) extends ((_: any, ..._1: infer Next) => any) ? Last<Next> : Default,
+  empty: Default
+  single: Tuple extends [infer SoleElement] ? SoleElement : never
+  multi: ((..._: Tuple) => any) extends ((_: any, ..._1: infer Next) => any) ? Last<Next> : Default
   infinite: Tuple extends (infer Element)[] ? Element : never
 }[
   Tuple extends [] ? 'empty' :
@@ -54,9 +54,9 @@ export type Reverse<Tuple extends any[], Prefix extends any[] = []> = {
   empty: Prefix,
   nonEmpty: ((..._: Tuple) => any) extends ((_: infer First, ..._1: infer Next) => any)
     ? Reverse<Next, Prepend<Prefix, First>>
-    : never,
+    : never
   infinite: {
-    ERROR: 'Cannot reverse an infinite tuple',
+    ERROR: 'Cannot reverse an infinite tuple'
     CODENAME: 'InfiniteTuple'
   }
 }[
@@ -66,13 +66,13 @@ export type Reverse<Tuple extends any[], Prefix extends any[] = []> = {
 ]
 
 export type Concat<Left extends any[], Right extends any[]> = {
-  emptyLeft: Right,
+  emptyLeft: Right
   singleLeft: Left extends [infer SoleElement]
     ? Prepend<Right, SoleElement>
-    : never,
+    : never
   multiLeft: ((..._: Reverse<Left>) => any) extends ((_: infer LeftLast, ..._1: infer ReversedLeftRest) => any)
     ? Concat<Reverse<ReversedLeftRest>, Prepend<Right, LeftLast>>
-    : never,
+    : never
   infiniteLeft: {
     ERROR: 'Left is not finite',
     CODENAME: 'InfiniteLeft' & 'Infinite'
@@ -88,8 +88,8 @@ export type Repeat<Type, Count extends number, Holder extends any[] = []> =
   number extends Count
     ? Type[]
     : {
-        fit: Holder,
-        unfit: Repeat<Type, Count, Prepend<Holder, Type>>,
+        fit: Holder
+        unfit: Repeat<Type, Count, Prepend<Holder, Type>>
         union: Count extends Holder['length'] | infer Rest ?
           Rest extends number ?
             Repeat<Type, Holder['length']> | Repeat<Type, Rest>
@@ -104,14 +104,14 @@ export type Repeat<Type, Count extends number, Holder extends any[] = []> =
       ]
 
 export type ConcatMultiple<TupleSet extends any[][]> = {
-  empty: [],
+  empty: []
   nonEmpty: ((..._: Reverse<TupleSet>) => any) extends ((_: infer Last, ..._1: infer ReversedRest) => any) ?
     Last extends any[] ?
     ReversedRest extends any[][] ?
       Concat<ConcatMultiple<Reverse<ReversedRest>>, Last> :
     never :
     never :
-    never,
+    never
   infinite: {
     ERROR: 'TupleSet is not finite',
     CODENAME: 'InfiniteTupleSet' & 'Infinite'
@@ -121,10 +121,10 @@ export type ConcatMultiple<TupleSet extends any[][]> = {
 ]
 
 export type SingleTupleSet<Types extends any[], Holder extends [any][] = []> = {
-  empty: Holder,
+  empty: Holder
   nonEmpty: ((..._: Reverse<Types>) => any) extends ((_: infer Last, ..._1: infer ReversedRest) => any)
     ? SingleTupleSet<Reverse<ReversedRest>, Prepend<Holder, [Last]>>
-    : never,
+    : never
   infinite: SplitInfiniteTuple<Types> extends [infer Finite, infer Infinite] ?
     Finite extends any [] ?
     Infinite extends (infer RepeatedElement)[] ?
@@ -137,19 +137,19 @@ export type SingleTupleSet<Types extends any[], Holder extends [any][] = []> = {
 ]
 
 export type FillTuple<Tuple extends any[], Replacement, Holder extends any[] = []> = {
-  empty: Holder,
+  empty: Holder
   nonEmpty: ((...a: Tuple) => any) extends ((a: infer First, ...b: infer Rest) => any)
     ? FillTuple<Rest, Replacement, Prepend<Holder, Replacement>>
-    : never,
+    : never
   infinite: Replacement[]
 }[
   Tuple extends [] ? 'empty' : IsFinite<Tuple, 'nonEmpty', 'infinite'>
 ]
 
 export type CompareLength<Left extends any[], Right extends any[]> = {
-  fitBoth: 'equal',
-  fitLeft: 'shorterLeft',
-  fitRight: 'shorterRight',
+  fitBoth: 'equal'
+  fitLeft: 'shorterLeft'
+  fitRight: 'shorterRight'
   unfit: ((..._: Left) => any) extends ((_: any, ..._1: infer LeftRest) => any) ?
     ((..._: Right) => any) extends ((_: any, ..._1: infer RightRest) => any) ?
       CompareLength<LeftRest, RightRest>
@@ -163,8 +163,8 @@ export type CompareLength<Left extends any[], Right extends any[]> = {
 ]
 
 export type SortTwoTuple<Left extends any[], Right extends any[], WhenEqual = [Left, Right]> = {
-  equal: WhenEqual,
-  shorterLeft: [Left, Right],
+  equal: WhenEqual
+  shorterLeft: [Left, Right]
   shorterRight: [Right, Left]
 }[CompareLength<Left, Right>]
 
@@ -186,7 +186,7 @@ export type ShortestTuple<TupleSet extends any[][], Shortest = any[]> = {
 ]
 
 export type LongestTuple<TupleSet extends any[][], Longest = []> = {
-  empty: Longest,
+  empty: Longest
   nonEmpty: ((..._: TupleSet) => any) extends ((_: infer Head, ..._1: infer Tail) => any) ?
     Tail extends any[] ?
     Longest extends any[] ?
