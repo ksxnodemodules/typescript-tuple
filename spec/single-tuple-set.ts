9d809cac21db042a9b7d3ebd63b6d021b88f16cf
khai96_ <hvksmr1996@gmail.com>
2020-08-22 16:03:09 +0700

    Remove unusable APIs

--- spec/single-tuple-set.ts
+++
@@ -1,7 +0,0 @@
-import { compare } from 'static-type-assert'
-import { SingleTupleSet } from '..'
-
-compare<SingleTupleSet<[]>, []>('equal')
-compare<SingleTupleSet<[0, 1, 2]>, [[0], [1], [2]]>('equal')
-compare<SingleTupleSet<'x'[]>, ['x'][]>('equal')
-compare<SingleTupleSet<[0, ...'x'[]]>, [[0], ...['x'][]]>('equal')
